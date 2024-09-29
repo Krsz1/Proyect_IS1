a2228ec5db6bc73d420560413a55834e54fd7500
samuelhgp <165844544+samuelhgp@users.noreply.github.com>
2024-09-29 11:05:46 -0500

    los metdodos

--- src/main/java/backend/proyect_doctic_is1/Service/IPublicationsService.java
+++ src/main/java/backend/proyect_doctic_is1/Service/IPublicationsService.java
@@ -1,5 +1,12 @@
 package backend.proyect_doctic_is1.Service;
 
+import java.util.List;
+
+import backend.proyect_doctic_is1.Model.PublicationsModel;
+
 public interface IPublicationsService {
    
+    List<PublicationsModel> findAllbyTitle(String title);
+
+    List<PublicationsModel> listAll();
 }
