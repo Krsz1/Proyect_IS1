bb15557265ea6a7591f553e1998036e0150ad7e1
Krsz1 <krsna.gutierrez@uao.edu.co>
2024-09-29 11:19:22 -0500

    MetodosSamu

--- src/main/java/backend/proyect_doctic_is1/Repository/IPublicationsRepository.java
+++ src/main/java/backend/proyect_doctic_is1/Repository/IPublicationsRepository.java
@@ -1,11 +1,17 @@
 package backend.proyect_doctic_is1.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import backend.proyect_doctic_is1.Model.PublicationsModel;
 
 @Repository
 public interface IPublicationsRepository extends MongoRepository<PublicationsModel,String>{
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<PublicationsModel> findAllByTitle(String title);
 
 }
